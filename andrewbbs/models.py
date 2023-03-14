e8a01de5e076287a8ace5a996e1c9c251151d649
lostways <andrew@lostways.com>
2023-03-14 02:37:33 -0700

    updated migrations

--- andrewbbs/models.py
+++ andrewbbs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User, AbstractBaseUser, BaseUserManager, PermissionsMixin
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -26,7 +27,7 @@ class Screen(models.Model):
     slug = models.SlugField(max_length=300, unique=True)
     published = models.BooleanField(default=False)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
                                related_name='screens')
 
     created_at = models.DateTimeField(auto_now_add=True)
