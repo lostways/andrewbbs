ecf96533bc968c1cd0fb102b9b6b34e165ce8580
lostways <andrew@lostways.com>
2023-03-31 19:50:22 -0700

    more production docker

--- docker/production/django/entrypoint.sh
+++ docker/production/django/entrypoint.sh
@@ -7,16 +7,7 @@ python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Applying database migrations"
-until psql $DATABASE_URL -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - continuing"
-
-if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
-  python manage.py migrate --noinput
-fi
+python manage.py migrate --noinput
 
 # Run command
 exec "$@"
