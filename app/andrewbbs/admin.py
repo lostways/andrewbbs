a1dd8cef7e4b27d35fc3237570769fb2c5e2c6c2
lostways <andrew@lostways.com>
2023-04-08 19:59:47 -0700

    filter by group, added joined on date on admin

--- app/andrewbbs/admin.py
+++ app/andrewbbs/admin.py
@@ -15,9 +15,18 @@ class AccessCodeAdmin(admin.ModelAdmin):
     search_fields = ('title', 'body')
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('handle', 'phone', 'first_name', 'last_name', 'zip')
+    list_display = ('handle','joined_on', 'phone', 'first_name', 'last_name', 'zip', 'get_groups', 'is_active', 'is_staff', 'is_superuser')
     search_fields = ('handle', 'phone', 'first_name', 'last_name', 'zip')
     readonly_fields = ('unlocked_codes',)
+    list_filter = ('groups',)
+
+    # joined on is created at time at YYYY-MM-DD HH:MM:SS
+    joined_on = lambda self, obj: obj.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    joined_on.short_description = 'Joined On'
+
+    # get groups is a comma separated list of groups
+    get_groups = lambda self, obj: ", ".join([g.name for g in obj.groups.all()])
+    get_groups.short_description = 'Groups'
 
 admin.site.register(Screen, ScreenAdmin)
 admin.site.register(AccessCode, AccessCodeAdmin)
